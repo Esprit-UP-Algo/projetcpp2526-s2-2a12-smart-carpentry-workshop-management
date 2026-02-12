f26339bb0f7d6dcd721eda2970544be27ee3979b
alcoholic-lain <iwakura.lain.pa@gmail.com>
2026-02-12 13:11:50 +0100

    Initial commit from wf branch

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_WoodFlow_FILE /home/lain/wf/projetcpp2526-s2-2a12-smart-carpentry-workshop-management-employer-wf/build/WoodFlow)
-set(__QT_DEPLOY_TARGET_WoodFlow_TYPE EXECUTABLE)
